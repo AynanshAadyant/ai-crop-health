046ddd36212f1c81faacab3ad4b6b9049773512c
AynanshAadyant <aynaadyant@gmail.com>
2025-09-17 21:09:15 +0530

    Corrected route to give out data regarding a particular crop from DB based on its ID

--- src/routes/crop.routes.js
+++ src/routes/crop.routes.js
@@ -1,12 +1,12 @@
 import e from "express";
 import { protectedRoute } from "../middlewares/jwt.middleware.js"
-import { addCrop, getAllCrops, removeCrop } from "../controllers/crop.controller.js";
+import { addCrop, getAllCrops, getCropById, removeCrop } from "../controllers/crop.controller.js";
 
 const Router = e.Router();
 
 Router.post( "/add", protectedRoute, addCrop );
 Router.get( "/", protectedRoute, getAllCrops );
-Router.get( "/:id", protectedRoute, getAllCrops );
+Router.get( "/:id", protectedRoute, getCropById );
 Router.delete( "/:id", protectedRoute, removeCrop );
 
 export default Router;
