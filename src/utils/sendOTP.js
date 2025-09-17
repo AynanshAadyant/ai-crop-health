28b058a8a129db1405845370feb4cccf328ec4c2
AynanshAadyant <aynaadyant@gmail.com>
2025-09-17 06:17:10 +0530

    Pushing to prod, hope it works, go lazy red panda

--- src/utils/sendOTP.js
+++ src/utils/sendOTP.js
@@ -1,42 +1,49 @@
-// import { Vonage } from "@vonage/server-sdk";
-// import { Channels } from "@vonage/messages;"
-// import dotenv from "dotenv";
+import { Client, Messaging } from 'node-appwrite';
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const vonage = new Vonage({
-//     apiKey : process.env.VONAGE_API_KEY,
-//     apiSecret : process.env.VONAGE_API_SECRET
-// })
+export const client = new Client();
+client
+    .setEndpoint("https://fra.cloud.appwrite.io/v1") // or your self-hosted URL
+    .setProject(process.env.APPWRITE_PROJECT_KEY)
+    .setKey(process.env.APPWRITE_API_SECRET);
 
-export default async function sendOTP( phoneNumber, otp ) {
-    // try{
-    //     const from = "CropRaksha";
-    //     const to = phoneNumber
-    //     const text = `Here's you OTP for logging into FarmGaurd\nOTP : ${otp}`
+const messaging = new Messaging(client);
 
-    //     const response = await vonage.sms.send({ messageType : 'sms', channel : Channels.SMS, to, from, text })
-    //     console.log( response );
-    //     return {
-    //         success: true,
-    //         message: "SMS sent successfully",
-    //         body: response,
-    //         status: 200
-    //     }
-    // }
-    // catch( e ) {
-    //     return {
-    //         success: false, 
-    //         status: 500,
-    //         message: "Error while sending SMS"
-    //     }
-    // }
+dotenv.config();
+export default async function sendOTP(phoneNumber, otp) {
+  const response = fallback( phoneNumber, otp );
+  return response;
+}
 
-    console.log( phoneNumber, "  OTP : " , otp );
-    return { success: true,
-        status: 200,
-        message: "OTP sent successfully"
-    }
+async function usingService( phoneNumber, otp ) {
+  try {
+    // const response = await messaging.createSms(
+    //   process.env.APPWRITE_SMS_TEMPLATE_ID,  // ✅ ID of SMS template from console
+    //   [phoneNumber],                        // ✅ must be array
+    //   undefined,                            // topics (not needed here)
+    //   { otp, expiry: 5 }                    // ✅ inject variables
 
+    // );
 
+    const response = await messaging.createSMS(
+      undefined,
+      [phoneNumber],
+      `Your otp is ${otp} `
+    )
+    console.log("SMS Sent:", response);
+  } catch (error) {
+    console.error("Error sending SMS:", error);
+  }
+}
+
+function fallback( phoneNumber, otp ) {
+  console.log( `Phone number :${phoneNumber}, otp : ${otp}`)
+  const response = {
+    success: true,
+    status: 200,
+    message: "OTP sent"
+  }
+  return response;
 }
