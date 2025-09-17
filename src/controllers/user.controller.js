836522453e0e6069603a20a178bf8108a09eee65
AynanshAadyant <aynaadyant@gmail.com>
2025-09-17 18:09:41 +0530

    OTP function issue resolved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -164,8 +164,8 @@ const login = async( req, res ) => {
         })
         const response = sendOTP( phoneNumber, otp );
         if( !response.success ) {
-            return res.status( response.status ).json( {
-                message: response.message,
+            return res.status( 500 ).json( {
+                message: response.message || "Something went wrong while sending otp",
                 success: false
             })
         }
